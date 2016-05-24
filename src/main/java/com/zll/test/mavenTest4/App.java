5b677a7cee57555f7429526dbe2393d4878ab4a3
zll77 <1320683061@qq.com>
2016-05-24 17:43:27 +0800

    first commit

---
+++ src/main/java/com/zll/test/mavenTest4/App.java
@@ -0,0 +1,13 @@
+package com.zll.test.mavenTest4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
